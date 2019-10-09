97b18dec5cc6c6bbc381b8b2b9939a40034d9296
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 05:49:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,25 @@
 # code your #position_taken? method here!
+
+#board = ["", " ", " ", " ", " ", " ", " ", " ", " "]
+#board = [nil, " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+
+def position_taken(i)
+  board = ["", " ", " ", " ", " ", " ", " ", " ", " "]
+  puts i
+  puts board[i]
+  if (board[i] == "X") || (board[i] == "O")
+    puts "true"
+    return true
+  else
+    puts false
+    return false
+  end  
+end
+
+i = 0
+while i < 9
+  puts i
+  position_taken(i)
+  i+=1
+end
