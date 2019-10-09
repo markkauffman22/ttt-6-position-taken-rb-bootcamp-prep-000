782c6ce680cb536654c964e11b7659383f70a532
Mark Kauffman <markkauffman22@gmail.com>
2019-10-09 05:50:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 # code your #position_taken? method here!
 
-#board = ["", " ", " ", " ", " ", " ", " ", " ", " "]
-#board = [nil, " ", " ", " ", " ", " ", " ", " ", " "]
-board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+
 
 def position_taken(i)
 
